d860e9d395d984af3272f0d9cfc61066989bb815
ismaelparejaramirez <ismaelparejaramz@gmail.com>
2020-08-07 18:10:07 +0200

    force deploy

--- deploy.php
+++ deploy.php
@@ -42,7 +42,7 @@ foreach ($allowed_ips as $allow) {
     }
 }
 
-if (!$allowed) {
+if (true) {
     header('HTTP/1.1 403 Forbidden');
     echo "<span style=\"color: #ff0000\">Sorry, no hamster - better convince your parents!</span>\n";
     echo "</pre>\n</body>\n</html>";
