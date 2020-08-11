36d57360c3d6b43a19ddb11794d5798a01f85e40
ismaelparejaramirez <ismaelparejaramz@gmail.com>
2020-08-11 19:59:05 +0200

    fix js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,15 @@
-$('.message a').click(function(){
-    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
-    
- });
+var buttonToLogin = document.getElementById('sign-in');
+var buttonToRegister = document.getElementById('register');
+
+var formRegister = document.getElementById('register-form');
+var formLogin = document.getElementById('login-form');
+
+buttonToLogin.addEventListener('click', () => {
+    formLogin.style.display = 'block';
+    formRegister.style.display = 'none';
+});
+
+buttonToRegister.addEventListener('click', () => {
+    formLogin.style.display = 'none';
+    formRegister.style.display = 'block';
+});
