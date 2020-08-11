20129bcd83d26f1f040372fad5344744a83f5709
apedrupayo <mrcaggy@gmail.com>
2020-08-11 20:40:43 +0200

    create entity user and register user services

---
+++ src/User/User/Domain/Services/RegisterUser.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace MusicProject\User\User\Domain\Services;
+
+use MusicProject\User\User\Domain\User;
+
+class RegisterUser
+{
+    public function __construct()
+    {
+        
+    }
+
+    public function __invoke(string $username, string $password, string $email)
+    {
+        $user = new User($username, $password, $email);
+    }
+}
--- src/User/User/Domain/User.php
+++ src/User/User/Domain/User.php
@@ -4,5 +4,14 @@ namespace MusicProject\User\User\Domain;
 
 class User
 {
+    private string $username;
+    private string $password;
+    private string $email;
 
+    public function __construct(string $username, string $password, string $email)
+    {
+        $this->username = $username;
+        $this->password = $password;
+        $this->email = $email;
+    }
 }
