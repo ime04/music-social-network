6b0eb4b3014812aa8bb6e71366c2716bc1995241
ismaelparejaramirez <ismaelparejaramz@gmail.com>
2020-08-12 17:12:54 +0200

    ad di container and create empty container

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
 
 include_once('./vendor/autoload.php');
+
+use \DI\ContainerBuilder;
+
+try {
+    global $container;
+    $container = new ContainerBuilder();
+} catch (Exception $exception) {
+    echo 'exception : ' . $exception;
+}
