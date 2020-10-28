cef4cfd7900d58fa09420e6660dfa61c5b00a920
ismaelparejaramirez <ismaelparejaramz@gmail.com>
2020-10-28 20:58:01 +0100

    first version for routing

--- index.php
+++ index.php
@@ -14,22 +14,23 @@ use MusicProject\User\User\Infrastructure\Controllers\RegisterUserAction;
 try
 {
     // Init basic route
+    //TODO Load routes in other file
     $userRegisterRoute = new Route(
         '/user/register',
         array('controller' => RegisterUserAction::class)
     );
 
     // Init route with dynamic placeholders
-    $foo_placeholder_route = new Route(
+    /*$foo_placeholder_route = new Route(
         '/foo/{id}',
         array('controller' => 'FooController', 'method'=>'load'),
         array('id' => '[0-9]+')
-    );
+    );*/
 
     // Add Route object(s) to RouteCollection object
     $routes = new RouteCollection();
     $routes->add('user_register', $userRegisterRoute);
-    $routes->add('foo_placeholder_route', $foo_placeholder_route);
+    //$routes->add('foo_placeholder_route', $foo_placeholder_route);
 
     // Init RequestContext object
     $context = new RequestContext();
@@ -42,16 +43,13 @@ try
     $parameters = $matcher->match($context->getPathInfo());
 
     // How to generate a SEO URL
-    $generator = new UrlGenerator($routes, $context);
+    /*$generator = new UrlGenerator($routes, $context);
     $url = $generator->generate('foo_placeholder_route', array(
         'id' => 123,
-    ));
-
-    echo '<pre>';
-    print_r($parameters);
-
-    echo 'Generated URL: ' . $url;
-    exit;
+    ));*/
+    
+    $response = (new $parameters['controller'])->__invoke();
+    $response->send();
 }
 catch (ResourceNotFoundException $e)
 {
