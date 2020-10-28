939074c7af7739f288c01a56f31fd83a4ff0f679
ismaelparejaramirez <ismaelparejaramz@gmail.com>
2020-10-28 18:42:17 +0100

    first version for routing

--- index.php
+++ index.php
@@ -2,17 +2,17 @@
 
 include_once('./vendor/autoload.php');
 
-use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
-use MusicProject\User\User\Infrastructure\Controllers\RegisterUserAction;
+use Symfony\Component\HttpFoundation\Request;
 
-return function (RoutingConfigurator $routes) {
-    $routes->add('register', '/user/register')
-        // the controller value has the format [controller_class, method_name]
-        ->controller([RegisterUserAction::class])
-        ->methods(['POST'])
+$request = Request::createFromGlobals();
 
-        // if the action is implemented as the __invoke() method of the
-        // controller class, you can skip the 'method_name' part:
-        // ->controller(BlogController::class)
-    ;
-};
+switch($request->getPathInfo()) {
+    case '/':
+        echo 'This is the home page';
+        break;
+    case '/user/register':
+        echo 'This is the user register page';
+        break;
+    default:
+        echo 'Not found!';
+}
