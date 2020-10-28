1ea6bdbfb8d6f7b90bc789de2ab8a0af5b7bf821
ismaelparejaramirez <ismaelparejaramz@gmail.com>
2020-10-28 18:30:22 +0100

    some test

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ use Symfony\Component\Routing\RouteCollection;
 use Symfony\Component\Routing\RequestContext;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Matcher\UrlMatcher;
+use MusicProject\User\User\Infrastructure\Controllers\RegisterUserAction;
 
 try {
     $registerUserRoute = new Route(
@@ -15,7 +16,10 @@ try {
         array('controller' => 'RegisterUserAction')
     );
     $routes = new RouteCollection();
-    $routes->add('register-user', $registerUserRoute);
+    $routes->add('register', '/user/register')
+        // the controller value has the format [controller_class, method_name]
+        ->controller([RegisterUserAction::class])
+        ->methods(['POST']);
     $context = new RequestContext();
     $context->fromRequest(Request::createFromGlobals());
     $matcher = new UrlMatcher($routes, $context);
