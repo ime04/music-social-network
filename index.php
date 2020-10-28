ad0d3db541d01fb1fc20b1f9bf02d9ccf8113c55
ismaelparejaramirez <ismaelparejaramz@gmail.com>
2020-10-28 19:38:12 +0100

    other test

--- index.php
+++ index.php
@@ -5,11 +5,23 @@ error_reporting(E_ALL);
 include_once('./vendor/autoload.php');
 
 use Symfony\Component\HttpFoundation\Request;
-use MusicProject\Core\Infrastructure\Config\Core;
-use MusicProject\User\User\Infrastructure\Controllers\RegisterUserAction;
+use Symfony\Component\HttpFoundation\Response;
 
 $request = Request::createFromGlobals();
-$app = new Core();
-$app->map('/user/register', RegisterUserAction::class);
-$response = $app->handle($request);
+
+$map = [
+    '/user/register' => 'RegisterUserAction',
+    '/bye'   => 'bye',
+];
+
+$path = $request->getPathInfo();
+if (isset($map[$path])) {
+    ob_start();
+    extract($request->query->all(), EXTR_SKIP);
+    include sprintf(__DIR__. '/src/pages/%s.php', $map[$path]);
+    $response = new Response(ob_get_clean());
+} else {
+    $response = new Response('Not Found', 404);
+}
+
 $response->send();
