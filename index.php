d91f7d899b275c0ef923dfae665c5aa1c2edd2fb
ismaelparejaramirez <ismaelparejaramz@gmail.com>
2020-10-28 18:32:59 +0100

    some test

--- index.php
+++ index.php
@@ -2,28 +2,17 @@
 
 include_once('./vendor/autoload.php');
 
-use Symfony\Component\Routing\Route;
-use Symfony\Component\Routing\Exception\ResourceNotFoundException;
-use Symfony\Component\Routing\RouteCollection;
-use Symfony\Component\Routing\RequestContext;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 use MusicProject\User\User\Infrastructure\Controllers\RegisterUserAction;
 
-try {
-    $registerUserRoute = new Route(
-        '/user/register',
-        array('controller' => 'RegisterUserAction')
-    );
-    $routes = new RouteCollection();
+return function (RoutingConfigurator $routes) {
     $routes->add('register', '/user/register')
         // the controller value has the format [controller_class, method_name]
         ->controller([RegisterUserAction::class])
-        ->methods(['POST']);
-    $context = new RequestContext();
-    $context->fromRequest(Request::createFromGlobals());
-    $matcher = new UrlMatcher($routes, $context);
-    $parameters = $matcher->match($context->getPathInfo());
-} catch (ResourceNotFoundException $e) {
-    echo $e->getMessage();
-}
+        ->methods(['POST'])
+
+        // if the action is implemented as the __invoke() method of the
+        // controller class, you can skip the 'method_name' part:
+        // ->controller(BlogController::class)
+    ;
+};
