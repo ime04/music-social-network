1478724baf12e695932cfa8d648e5a87f4093a75
ismaelparejaramirez <ismaelparejaramz@gmail.com>
2020-10-28 19:39:18 +0100

    other test

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ $path = $request->getPathInfo();
 if (isset($map[$path])) {
     ob_start();
     extract($request->query->all(), EXTR_SKIP);
-    include sprintf(__DIR__. '/src/pages/%s.php', $map[$path]);
+    include sprintf(__DIR__. '/src/User/User/Infrastructure/Controllers/%s.php', $map[$path]);
     $response = new Response(ob_get_clean());
 } else {
     $response = new Response('Not Found', 404);
