0efdc8763afba9c62f6e1ffb141e5a41a2935d18
ismaelparejaramirez <ismaelparejaramz@gmail.com>
2020-10-28 18:49:12 +0100

    create Core.php and call from index

--- index.php
+++ index.php
@@ -3,23 +3,8 @@
 include_once('./vendor/autoload.php');
 
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-
+use MusicProject\Core\Infrastructure\Config\Core;
 $request = Request::createFromGlobals();
-$response = new Response();
-
-switch ($request->getPathInfo()) {
-    case '/':
-        $response->setContent('This is the website home');
-        break;
-
-    case '/user/register':
-        $response->setContent('This is the user register page');
-        break;
-
-    default:
-        $response->setContent('Not found !');
-        $response->setStatusCode(Response::HTTP_NOT_FOUND);
-}
-
+$app = new Core();
+$response = $app->handle($request);
 $response->send();
