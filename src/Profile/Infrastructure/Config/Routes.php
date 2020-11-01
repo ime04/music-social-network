023b905c80ebaf8e335637320ecd3f2e903e29ee
ismaelparejaramirez <ismaelparejaramz@gmail.com>
2020-11-01 14:31:43 +0100

    other test

--- src/Profile/Infrastructure/Config/Routes.php
+++ src/Profile/Infrastructure/Config/Routes.php
@@ -4,13 +4,10 @@ namespace MusicProject\Profile\User\Infrastructure\Config;
 
 use MusicProject\Profile\User\Infrastructure\Controllers\RegisterUserAction;
 use MusicProject\Core\Infrastructure\Routes\CreateRoute;
-use Psr\Container\ContainerInterface;
 
 return [
     'userRegister' => [
         'name' => 'user-register',
-        'route' => function (ContainerInterface $container) {
-            return $container->call(CreateRoute::class, ['/user/register', RegisterUserAction::class, ['POST']]);
-        }
+        'route' => (new CreateRoute())->__invoke('/user/register', RegisterUserAction::class, ['POST'])
     ]
 ];
