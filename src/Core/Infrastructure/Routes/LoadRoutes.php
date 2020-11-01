a16cc076ba6ecf21ee68d52afb889bf2c5305d25
ismaelparejaramirez <ismaelparejaramz@gmail.com>
2020-11-01 14:16:59 +0100

    fix include path

--- src/Core/Infrastructure/Routes/LoadRoutes.php
+++ src/Core/Infrastructure/Routes/LoadRoutes.php
@@ -4,10 +4,10 @@ namespace MusicProject\Core\Infrastructure\Routes;
 
 class LoadRoutes
 {
-    public function __invoke()
+    public function __invoke() : array
     {
         return array_merge(
-            include(__DIR__ . '../../../Profile/Infrastructure/Config/Routes.php')
+            include(__DIR__ . '/../../../Profile/Infrastructure/Config/Routes.php')
         );
     }
 }
