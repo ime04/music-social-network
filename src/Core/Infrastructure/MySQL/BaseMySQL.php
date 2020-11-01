ce6cd87f8c54608121fd020d480d6b28d56f7c8d
ismaelparejaramirez <ismaelparejaramz@gmail.com>
2020-11-01 21:14:31 +0100

    some refactor BaseMySQL.php

--- src/Core/Infrastructure/MySQL/BaseMySQL.php
+++ src/Core/Infrastructure/MySQL/BaseMySQL.php
@@ -9,21 +9,31 @@ use ClanCats\Hydrahon\Query\Sql\FetchableInterface;
 
 class BaseMySQL
 {
+    private const HOST = 'localhost';
+    private const DATABASE = 'MusicSocialNetWork';
+    private const USERNAME = 'victor';
+    private const PASSWORD = '=PM~U.MfJvg%k2L}';
+
     protected Builder $builderMySQL;
 
     public function __construct()
     {
         try {
-            $connection = new PDO('mysql:host=localhost;dbname=MusicSocialNetWork', 'victor', '=PM~U.MfJvg%k2L}');
-            $this->builderMySQL = new Builder('mysql', function($query, $queryString, $queryParameters) use($connection)
-            {
-                $statement = $connection->prepare($queryString);
-                $statement->execute($queryParameters);
-                if ($query instanceof FetchableInterface)
-                {
-                    return $statement->fetchAll(PDO::FETCH_ASSOC);
+            $connection = new PDO(
+                sprintf('mysql:host=%s;dbname=%s', self::HOST, self::DATABASE),
+                self::USERNAME,
+                self::PASSWORD
+            );
+            $this->builderMySQL = new Builder(
+                'mysql',
+                function($query, $queryString, $queryParameters) use ($connection) {
+                    $statement = $connection->prepare($queryString);
+                    $statement->execute($queryParameters);
+                    if ($query instanceof FetchableInterface) {
+                        return $statement->fetchAll(PDO::FETCH_ASSOC);
+                    }
                 }
-            });
+             );
         } catch(PDOException $ex){
             die(json_encode(array('outcome' => false, 'message' => 'Unable to connect')));
         }
