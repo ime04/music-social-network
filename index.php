eacbf695f26a9e0c07c2b86cab094658595f8920
ismaelparejaramirez <ismaelparejaramz@gmail.com>
2020-11-01 12:48:02 +0100

    remove services unused, add container di for load dependencies

--- index.php
+++ index.php
@@ -11,22 +11,17 @@ use Symfony\Component\Routing\RequestContext;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 use MusicProject\Core\Infrastructure\Routes\Routes;
-use MusicProject\Core\Infrastructure\Config\CommonContainer;
-use MusicProject\Core\Infrastructure\Services\Setup;
 
 try
 {
     global $container;
-    //$container = (new Setup(new CommonContainer()))->getContainer();
     $container = new DI\Container();
     $routes = (new Routes())->__invoke();
     $context = new RequestContext();
     $context->fromRequest(Request::createFromGlobals());
     $matcher = new UrlMatcher($routes, $context);
     $parameters = $matcher->match($context->getPathInfo());
-    //$response = (new $parameters['controller'])->__invoke();
     $controller = $container->get($parameters['controller']);
-    var_dump($controller);
     if (method_exists($controller, '__invoke')) {
         $response = $controller->__invoke();
         $response->send();
