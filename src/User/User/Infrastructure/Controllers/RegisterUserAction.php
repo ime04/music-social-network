61b4c34d57dbf190434e55bc26e1736773173353
ismaelparejaramirez <ismaelparejaramz@gmail.com>
2020-11-01 12:01:41 +0100

    trace for request object

--- src/User/User/Infrastructure/Controllers/RegisterUserAction.php
+++ src/User/User/Infrastructure/Controllers/RegisterUserAction.php
@@ -3,11 +3,21 @@
 namespace MusicProject\User\User\Infrastructure\Controllers;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 
 class RegisterUserAction
 {
+    private Request $request;
+
+    public function __construct(
+        Request $request
+    ) {
+        $this->request = $request;
+    }
+
     public function __invoke() : Response
     {
+        var_dump($this->request);
         return new Response('entraaaaaaa');
     }
 }
