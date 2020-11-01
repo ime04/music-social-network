241f0b72ebe0c92ee1029721084bb727b18eab34
ismaelparejaramirez <ismaelparejaramz@gmail.com>
2020-11-01 12:40:49 +0100

    some tests

--- index.php
+++ index.php
@@ -17,14 +17,16 @@ use MusicProject\Core\Infrastructure\Services\Setup;
 try
 {
     global $container;
-    $container = (new Setup(new CommonContainer()))->getContainer();
+    //$container = (new Setup(new CommonContainer()))->getContainer();
+    $container = new DI\Container();
     $routes = (new Routes())->__invoke();
     $context = new RequestContext();
     $context->fromRequest(Request::createFromGlobals());
     $matcher = new UrlMatcher($routes, $context);
     $parameters = $matcher->match($context->getPathInfo());
     $response = (new $parameters['controller'])->__invoke();
-    $controller = $this->container->get($parameters['controller']);
+    $controller = $container->get($parameters['controller']);
+    var_dump($controller);
     if (method_exists($controller, '__invoke')) {
         return $controller->__invoke();
     }
