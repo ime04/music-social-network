ad993531367303217631ffa6fe9b539200db4b8f
victorsgmii <victorsgmii@gmail.com>
2020-11-02 17:45:58 +0100

    prueba

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,8 +1,9 @@
-var buttonToLogin = document.getElementById('sign-in');
-var buttonToRegister = document.getElementById('register');
+const buttonToLogin = document.getElementById('sign-in');
+const buttonToRegister = document.getElementById('register');
+const registerButton = document.getElementById('register-button');
 
-var formRegister = document.getElementById('register-form');
-var formLogin = document.getElementById('login-form');
+const formRegister = document.getElementById('register-form');
+const formLogin = document.getElementById('login-form');
 
 buttonToLogin.addEventListener('click', () => {
     formLogin.style.display = 'block';
@@ -13,3 +14,26 @@ buttonToRegister.addEventListener('click', () => {
     formLogin.style.display = 'none';
     formRegister.style.display = 'block';
 });
+
+//INPUT CONST
+const nameField = document.getElementById('name-field');
+const passwordField = document.getElementById('password-field');
+const repeatPasswordField = document.getElementById('repeat-password-field');
+const emailField = document.getElementById('email-field');
+const errorElement = document.getElementById('error');
+
+formRegister.addEventListener('submit', (e) =>{
+    let errorMessages = [];
+    validateField(nameField)
+    validateField(passwordField)
+    validateField(repeatPasswordField)
+    validateField(emailField)
+})
+
+function validateField(field){
+    if(field.value === '' || field.value === null){
+        field.style.backgroundColor = '#df6464';
+    } else {
+        field.style.backgroundColor = '';
+    }
+}
