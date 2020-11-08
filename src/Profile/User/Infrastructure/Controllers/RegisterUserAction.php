9cccc38f06bb264f66c5ba903589863bd807247f
ismaelparejaramirez <ismaelparejaramz@gmail.com>
2020-11-08 16:18:06 +0100

    var dump request

--- src/Profile/User/Infrastructure/Controllers/RegisterUserAction.php
+++ src/Profile/User/Infrastructure/Controllers/RegisterUserAction.php
@@ -17,7 +17,7 @@ class RegisterUserAction
 
     public function __invoke() : Response
     {
-        var_dump($this->request->request->get('password'));
+        var_dump($this->request->request);
         return new Response('entraaaaaaa');
     }
 }
