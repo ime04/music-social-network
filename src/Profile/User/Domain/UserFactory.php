b11986920293ccb5d3df3f12c58394bd8e8db79d
ismaelparejaramirez <ismaelparejaramz@gmail.com>
2021-01-29 19:27:08 +0100

    set property to user factory

--- src/Profile/User/Domain/UserFactory.php
+++ src/Profile/User/Domain/UserFactory.php
@@ -3,16 +3,18 @@
 namespace MusicProject\Profile\User\Domain;
 
 use MusicProject\Shared\Domain\Entity\EntityFactoryInterface;
+use MusicProject\Shared\ValueObjects\Email\Email;
+use MusicProject\Shared\ValueObjects\Password\Password;
+use MusicProject\Shared\ValueObjects\Username\Username;
 
 class UserFactory implements EntityFactoryInterface
 {
     public function fromArray(array $data) : User
     {
         return new User(
-            //$data['id'],
-            $data['username'],
-            $data['password'],
-            $data['email']
+            new Username($data['username']),
+            new Password($data['password']),
+            new Email($data['email'])
         );
     }
 }
