818d377c5802b542d3e7b0e57ddae4ba028a4f51
ismaelparejaramirez <ismaelparejaramz@gmail.com>
2021-01-29 18:52:25 +0100

    some changes

--- src/Profile/User/Domain/User.php
+++ src/Profile/User/Domain/User.php
@@ -3,31 +3,34 @@
 namespace MusicProject\Profile\User\Domain;
 
 use MusicProject\Shared\Domain\Entity\EntityBase;
+use MusicProject\Shared\ValueObjects\Email\Email;
+use MusicProject\Shared\ValueObjects\Password\Password;
+use MusicProject\Shared\ValueObjects\Username\Username;
 
 class User extends EntityBase
 {
-    private string $username;
-    private string $password;
-    private string $email;
+    private Username $username;
+    private Password $password;
+    private Email $email;
 
-    public function __construct(string $username, string $password, string $email)
+    public function __construct(Username $username, Password $password, Email $email)
     {
         $this->username = $username;
         $this->password = $password;
         $this->email = $email;
     }
 
-    public function userName() : string
+    public function userName() : Username
     {
         return $this->username;
     }
 
-    public function password() : string
+    public function password() : Password
     {
         return $this->password;
     }
 
-    public function email() : string
+    public function email() : Email
     {
         return $this->email;
     }
