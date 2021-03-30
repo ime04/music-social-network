1fbbc1dea2548a2ef296bf5e244be738aa039982
ismaelparejaramirez <ismaelparejaramz@gmail.com>
2021-03-30 19:49:23 +0200

    rename

---
+++ src/Shared/Domain/Events/EventSubscribers.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace MusicProject\Shared\Infrastructure\Definitions;
+
+class EventSubscribers
+{
+    public function __invoke() : array
+    {
+        return array_merge(
+            include_once(__DIR__ . '../../../Profile/Config/EventsSubscribers.php')
+        );
+    }
+}
--- src/Shared/Infrastructure/Definitions/CommonDefinitions.php
+++ src/Shared/Infrastructure/Definitions/CommonDefinitions.php
@@ -2,10 +2,11 @@
 
 use Symfony\Component\HttpFoundation\Request;
 use MusicProject\Shared\Infrastructure\Events\InMemoryEventBus;
+use MusicProject\Shared\Infrastructure\Definitions\EventSubscribers;
 
 return [
     Request::class => Request::createFromGlobals(),
     InMemoryEventBus::class => new InMemoryEventBus(
-        (new EventsListeners())->__invoke()
+        (new EventSubscribers())->__invoke()
     )
 ];
