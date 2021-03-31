b01f8626ff0218724c51777b504d9524a57785f1
ismaelparejaramirez <ismaelparejaramz@gmail.com>
2021-03-31 16:26:04 +0200

    add interface definitions

--- src/Shared/Infrastructure/Definitions/CommonDefinitions.php
+++ src/Shared/Infrastructure/Definitions/CommonDefinitions.php
@@ -3,10 +3,11 @@
 use Symfony\Component\HttpFoundation\Request;
 use MusicProject\Shared\Infrastructure\Events\InMemoryEventBus;
 use MusicProject\Shared\Infrastructure\Definitions\EventSubscribers;
+use MusicProject\Shared\Domain\Events\EventBus;
 
 return [
     Request::class => Request::createFromGlobals(),
-    InMemoryEventBus::class => new InMemoryEventBus(
+    EventBus::class => new InMemoryEventBus(
         (new EventSubscribers())->__invoke()
     )
 ];
