a08453f6c5ff9844f5e85a7e4f95943e97b5100f
ismaelparejaramirez <ismaelparejaramz@gmail.com>
2021-04-01 11:56:02 +0200

    set trace in listener

--- src/Profile/User/Domain/Subscribers/SendEmailForUserRegistered.php
+++ src/Profile/User/Domain/Subscribers/SendEmailForUserRegistered.php
@@ -10,6 +10,8 @@ class SendEmailForUserRegistered implements DomainEventSubscriber
 
     public function __invoke(UserRegistered $event) : void
     {
+        echo "listener: \n";
+        var_dump($event);
         // TODO: send email for a new user
     }
 
